dd685ce9a856b8df6066208325e586de1a2277c2
plw <1165960540@qq.com>
2022-09-15 13:57:40 +0800

    标记结果显示

--- app.js
+++ app.js
@@ -5,13 +5,23 @@ app.use(express.static("static"))
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+// cros
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'Authorization,X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method')
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PATCH, PUT, DELETE')
+    res.header('Allow', 'GET, POST, PATCH, OPTIONS, PUT, DELETE')
+    next();
+});
+
 
 var server = app.listen(5000, function () {
     console.log('Listening on port %d', server.address().port);
 });
 
 const datasetPath = 'dataset/Geolife Trajectories 1.3/Data/001/Trajectory/'
-const resultsPath = 'results/result.json'
+// const resultsPath = 'results/result.json'
+const resultsPath = '/home/plw/dataset/GTMarker/001/g2.json'
 const load = require('./utils/load.js')
 
 app.get('/', function (req, res) {
@@ -20,6 +30,10 @@ app.get('/', function (req, res) {
 
 app.get('/contrast', function (req, res) {
     res.sendFile(__dirname + "/views/" + "contrast.html")
+})
+
+app.get('/showResult', function (req, res) {
+    res.sendFile(__dirname + "/views/" + "result.html")
 })
 
 app.post('/mark/point', function (req, res) {
